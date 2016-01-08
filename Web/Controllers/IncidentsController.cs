faa5be23d0ab190db401bea659dd8d708f95a3f0
Maximiliano Poggio <maxenz@gmail.com>
2016-01-07 23:37:33 -0300

    Agrego api controller incidentes

---
+++ Web/Controllers/IncidentsController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Shaman.Controllers
+{
+    public class IncidentsController : BaseApiController
+    {
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(IncidentDal.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+        }
+    }
+}
