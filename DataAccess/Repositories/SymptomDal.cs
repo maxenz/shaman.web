ffe05ffe0f601ca4512a041937f63b2a9f123de4
Maximiliano Poggio <maxenz@gmail.com>
2016-02-07 15:28:26 -0300

    Agrego popup de sintomas

---
+++ DataAccess/Repositories/SymptomDal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ShamanExpressDLL;
+using Domain;
+using Domain.Utils;
+using System.Data;
+
+namespace DataAccess.Repositories
+{
+    public static class SymptomDal
+    {
+        static conSintomas conSintomas;
+
+        static SymptomDal()
+        {
+            conSintomas = new conSintomas();
+        }
+
+        public static List<Symptom> GetAll()
+        {
+            DataTable symptons = conSintomas.GetAll();
+            return symptons.DataTableToList<Symptom>();
+        }
+    }
+}
---
+++ Web/Controllers/SymptomsController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess;
+using DataAccess.Repositories;
+using System;
+using System.Web.Http;
+
+namespace Shaman.Controllers
+{
+    public class SymptomsController : BaseApiController
+    {
+        [HttpGet]
+        [HttpOptions]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(SymptomDal.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
